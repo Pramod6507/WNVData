7cbb65315a291a18c3b025dc26f551bb477d3b44
Pramod Kumar <pramod6507@gmail.com>
2020-09-28 16:01:53 -0400

    Add project files.

---
+++ WNVData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WNVData.Startup))]
+namespace WNVData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
